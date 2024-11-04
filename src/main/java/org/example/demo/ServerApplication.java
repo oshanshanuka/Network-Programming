2e9789651c1df8c270e9ffd172e835db685cdd6f
oshan shanuka <oshanshanuka428@gmail.com>
2024-11-04 19:59:24 +0530

    first commit

---
+++ src/main/java/org/example/demo/ServerApplication.java
@@ -0,0 +1,20 @@
+package org.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class ServerApplication extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("server.fxml"))));
+        primaryStage.show();
+    }
+}
